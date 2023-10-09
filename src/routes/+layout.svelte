fb463c5caaea916e5629c5e5fec32e7a8692d832
Luiz Vicente <luizcv89@gmail.com>
2023-10-08 23:38:37 -0300

    Inject Vercel Analytics into the project pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
+  import { dev } from "$app/environment";
+  import { inject } from "@vercel/analytics";
   import "../app.postcss";
+
+  inject({ mode: dev ? "development" : "production" });
 </script>
 
 <slot />
