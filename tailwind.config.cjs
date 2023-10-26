dc7c77b4f4e91ddbfb46c84ef584db3677c585be
Luiz Vicente <luizcv89@gmail.com>
2023-10-26 15:34:39 -0300

    Add remaining packages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,27 @@
+import { join } from "path";
+import { skeleton } from "@skeletonlabs/tw-plugin";
+
+import forms from "@tailwindcss/forms";
+
 /** @type {import('tailwindcss').Config}*/
 const config = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
-
+  darkMode: "class",
+  content: [
+    "./src/**/*.{html,js,svelte,ts}",
+    join(
+      require.resolve("@skeletonlabs/skeleton"),
+      "../**/*.{html,js,svelte,ts}"
+    ),
+  ],
   theme: {
     extend: {},
   },
-
-  plugins: [],
+  plugins: [
+    forms,
+    skeleton({
+      themes: { preset: ["rocket"] },
+    }),
+  ],
 };
 
 module.exports = config;
