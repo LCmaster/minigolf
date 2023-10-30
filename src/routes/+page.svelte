9fc0671813f1e9b9997927f5bc2a503814273bcf
Luiz Vicente <luizcv89@gmail.com>
2023-10-29 23:05:51 -0300

    Fix home screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,29 +23,21 @@
 </script>
 
 <div class="bg-[#C4E9CC]">
-  {#if game}
-    <GameScreen bind:game on:quit={() => (game = null)} />
-  {:else}
-    <!-- <LobbyScreen
-      courses={data.courses}
-      on:submit={(ev) => handleGameStart(ev.detail)}
-    /> -->
-    <div
-      class="w-screen h-screen flex flex-col gap-6 justify-center items-center"
-    >
-      <img src="/logo.png" alt="Minigolf Mania" width="320" />
-      <div class="w-80 flex flex-col gap-2">
-        <Button on:click={() => modalStore.trigger(modal)}>How to play</Button>
-        <Button
-          on:click={() => {
-            window.location = "/game";
-          }}>Play</Button
-        >
-        <!-- 
+  <div
+    class="w-screen h-screen flex flex-col gap-6 justify-center items-center"
+  >
+    <img src="/logo.png" alt="Minigolf Mania" width="320" />
+    <div class="w-80 flex flex-col gap-2">
+      <Button on:click={() => modalStore.trigger(modal)}>How to play</Button>
+      <Button
+        on:click={() => {
+          window.location = "/game";
+        }}>Play</Button
+      >
+      <!-- 
         <img src="/separator.png" alt="or" />
         <Button>Sign In</Button>
         -->
-      </div>
     </div>
-  {/if}
+  </div>
 </div>
