1f2297c8638acebf217121502e215b71aa1b49d6
Luiz Vicente <luizcv89@gmail.com>
2023-10-30 17:22:29 -0300

    Remove supabase just for now

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,5 @@
 <script>
   import { dev } from "$app/environment";
-  import { invalidate } from "$app/navigation";
-  import { onMount } from "svelte";
   import { inject } from "@vercel/analytics";
 
   import { Modal, initializeStores } from "@skeletonlabs/skeleton";
@@ -17,21 +15,6 @@
   };
 
   export let data;
-
-  let { supabase, session } = data;
-  $: ({ supabase, session } = data);
-
-  onMount(() => {
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange((event, _session) => {
-      if (_session?.expires_at !== session?.expires_at) {
-        invalidate("supabase:auth");
-      }
-    });
-
-    return () => subscription.unsubscribe();
-  });
 </script>
 
 <Modal components={modalRegistry} />
