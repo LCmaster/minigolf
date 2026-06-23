bca1ce5b17d42b3c7601a9ed686c62cb9db5bce0
Luiz Vicente <luizcv89@gmail.com>
2026-06-23 18:53:50 -0300

    style: scale down grass tufts to match ball height

--- src/lib/scene/blocks/GrassBlock.svelte
+++ src/lib/scene/blocks/GrassBlock.svelte
@@ -15,27 +15,10 @@
 
 <!-- No physics, purely decorative visual element -->
 <T.Group {position} rotation={actualRotation} {scale}>
-  {#if variation === 1}
-    <!-- Single Small Tuft -->
-    <T.Group position={[0, 0, 0]}>
-      <T.Mesh material={grassMaterial} castShadow receiveShadow position={[0.2, 1.5, 0]} rotation={[0, 0, -0.2]}>
-        <T.ConeGeometry args={[0.5, 3.0, 4]} />
-      </T.Mesh>
-      <T.Mesh material={grassMaterial} castShadow receiveShadow position={[-0.2, 1.3, 0.2]} rotation={[0.2, 1.0, 0.2]}>
-        <T.ConeGeometry args={[0.4, 2.6, 4]} />
-      </T.Mesh>
-      <T.Mesh material={grassMaterial} castShadow receiveShadow position={[0, 1.7, -0.3]} rotation={[-0.2, 2.0, 0]}>
-        <T.ConeGeometry args={[0.6, 3.4, 4]} />
-      </T.Mesh>
-      <T.Mesh material={grassMaterial} castShadow receiveShadow position={[0.3, 1.2, 0.3]} rotation={[0.3, -1.0, -0.3]}>
-        <T.ConeGeometry args={[0.3, 2.4, 4]} />
-      </T.Mesh>
-    </T.Group>
-  {:else}
-    <!-- Wild Overgrown Patch (3 Tufts) -->
-    <T.Group position={[0, 0, 0]}>
-      <!-- Tuft 1 (Center) -->
-      <T.Group position={[0, 0, 0]} scale={[1, 1, 1]}>
+  <T.Group scale={[0.4, 0.4, 0.4]}>
+    {#if variation === 1}
+      <!-- Single Small Tuft -->
+      <T.Group position={[0, 0, 0]}>
         <T.Mesh material={grassMaterial} castShadow receiveShadow position={[0.2, 1.5, 0]} rotation={[0, 0, -0.2]}>
           <T.ConeGeometry args={[0.5, 3.0, 4]} />
         </T.Mesh>
@@ -45,27 +28,46 @@
         <T.Mesh material={grassMaterial} castShadow receiveShadow position={[0, 1.7, -0.3]} rotation={[-0.2, 2.0, 0]}>
           <T.ConeGeometry args={[0.6, 3.4, 4]} />
         </T.Mesh>
-      </T.Group>
-      
-      <!-- Tuft 2 (Left) -->
-      <T.Group position={[-1.5, 0, 0.5]} scale={[0.8, 0.8, 0.8]} rotation={[0, 1.5, 0]}>
-        <T.Mesh material={grassMaterial} castShadow receiveShadow position={[0.2, 1.5, 0]} rotation={[0, 0, -0.2]}>
-          <T.ConeGeometry args={[0.5, 3.0, 4]} />
-        </T.Mesh>
-        <T.Mesh material={grassMaterial} castShadow receiveShadow position={[-0.2, 1.3, 0.2]} rotation={[0.2, 1.0, 0.2]}>
-          <T.ConeGeometry args={[0.4, 2.6, 4]} />
+        <T.Mesh material={grassMaterial} castShadow receiveShadow position={[0.3, 1.2, 0.3]} rotation={[0.3, -1.0, -0.3]}>
+          <T.ConeGeometry args={[0.3, 2.4, 4]} />
         </T.Mesh>
       </T.Group>
+    {:else}
+      <!-- Wild Overgrown Patch (3 Tufts) -->
+      <T.Group position={[0, 0, 0]}>
+        <!-- Tuft 1 (Center) -->
+        <T.Group position={[0, 0, 0]} scale={[1, 1, 1]}>
+          <T.Mesh material={grassMaterial} castShadow receiveShadow position={[0.2, 1.5, 0]} rotation={[0, 0, -0.2]}>
+            <T.ConeGeometry args={[0.5, 3.0, 4]} />
+          </T.Mesh>
+          <T.Mesh material={grassMaterial} castShadow receiveShadow position={[-0.2, 1.3, 0.2]} rotation={[0.2, 1.0, 0.2]}>
+            <T.ConeGeometry args={[0.4, 2.6, 4]} />
+          </T.Mesh>
+          <T.Mesh material={grassMaterial} castShadow receiveShadow position={[0, 1.7, -0.3]} rotation={[-0.2, 2.0, 0]}>
+            <T.ConeGeometry args={[0.6, 3.4, 4]} />
+          </T.Mesh>
+        </T.Group>
+        
+        <!-- Tuft 2 (Left) -->
+        <T.Group position={[-1.5, 0, 0.5]} scale={[0.8, 0.8, 0.8]} rotation={[0, 1.5, 0]}>
+          <T.Mesh material={grassMaterial} castShadow receiveShadow position={[0.2, 1.5, 0]} rotation={[0, 0, -0.2]}>
+            <T.ConeGeometry args={[0.5, 3.0, 4]} />
+          </T.Mesh>
+          <T.Mesh material={grassMaterial} castShadow receiveShadow position={[-0.2, 1.3, 0.2]} rotation={[0.2, 1.0, 0.2]}>
+            <T.ConeGeometry args={[0.4, 2.6, 4]} />
+          </T.Mesh>
+        </T.Group>
 
-      <!-- Tuft 3 (Right) -->
-      <T.Group position={[1.2, 0, -0.8]} scale={[0.9, 0.9, 0.9]} rotation={[0, -1.0, 0]}>
-        <T.Mesh material={grassMaterial} castShadow receiveShadow position={[0.2, 1.5, 0]} rotation={[0, 0, -0.2]}>
-          <T.ConeGeometry args={[0.5, 3.0, 4]} />
-        </T.Mesh>
-        <T.Mesh material={grassMaterial} castShadow receiveShadow position={[0, 1.7, -0.3]} rotation={[-0.2, 2.0, 0]}>
-          <T.ConeGeometry args={[0.6, 3.4, 4]} />
-        </T.Mesh>
+        <!-- Tuft 3 (Right) -->
+        <T.Group position={[1.2, 0, -0.8]} scale={[0.9, 0.9, 0.9]} rotation={[0, -1.0, 0]}>
+          <T.Mesh material={grassMaterial} castShadow receiveShadow position={[0.2, 1.5, 0]} rotation={[0, 0, -0.2]}>
+            <T.ConeGeometry args={[0.5, 3.0, 4]} />
+          </T.Mesh>
+          <T.Mesh material={grassMaterial} castShadow receiveShadow position={[0, 1.7, -0.3]} rotation={[-0.2, 2.0, 0]}>
+            <T.ConeGeometry args={[0.6, 3.4, 4]} />
+          </T.Mesh>
+        </T.Group>
       </T.Group>
-    </T.Group>
-  {/if}
+    {/if}
+  </T.Group>
 </T.Group>
